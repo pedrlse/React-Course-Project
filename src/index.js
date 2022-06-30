5d13960291e9c9e31ac0de2ec229d5623f49c6fa
Pedro Mota <pedro.mota@exaud.com>
2022-06-30 17:05:50 +0100

    11.2 - changed html elements components

--- src/index.js
+++ src/index.js
@@ -16,14 +16,14 @@ import logo from "./react-logo.svg"
 const page = (
     <div>
         <img src={logo} width = "40px"/>
-        <h1>My awesome website in React</h1>
-        <h3>Reasons I love React</h3>
-        <ol>
-            <li>It´s composable</li>
-            <li>It´s declarative</li>
-            <li>It´s a hireable skill</li>
-            <li>It´s actively maintained by skilled people</li>
-        </ol>
+        <h1>Fun Facts about React</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>has well over 100k stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
     </div>
 )
 
