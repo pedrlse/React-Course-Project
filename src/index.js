eaa706aefe7a373726481200a466e477ba03b56e
Pedro Mota <pedro.mota@exaud.com>
2022-06-30 15:50:45 +0100

    9.2 - removed unnecessary code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
-// const h1 = document.createElement("h1")
-// h1.textContent = "Hello world"
-// h1.className = "header"
-// console.log(h1)
 
-// const page = (
-//     <div>
-//         <h1 className="header">This is JSX</h1>
-//         <p>This is a paragraph</p>
-//     </div>
-// )
 
 const navbar = (
     <nav>
