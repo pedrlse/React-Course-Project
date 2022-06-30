b2a1862f5fcb4fc62a4676fd2c71e220fe1aee50
Pedro Mota <pedro.mota@exaud.com>
2022-06-30 16:16:27 +0100

    10.1 - changed html element constant to display a list

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-const navbar = (
-    <nav>
-        <h1>Bob's Bistro</h1>
-        <ul>
-            <li>Menu</li>
-            <li>About</li>
-            <li>Contact</li>
-        </ul>
-    </nav>
+// const navbar = (
+//     <nav>
+//         <h1>Bob's Bistro</h1>
+//         <ul>
+//             <li>Menu</li>
+//             <li>About</li>
+//             <li>Contact</li>
+//         </ul>
+//     </nav>
+// )
+
+const page = (
+    <div>
+        <h1>My awesome website in React</h1>
+        <h3>Reasons I love React</h3>
+        <ol>
+            <li>It´s composable</li>
+            <li>It´s declarative</li>
+            <li>It´s a hireable skill</li>
+            <li>It´s actively maintained by skilled people</li>
+        </ol>
+    </div>
 )
 
-console.log(navbar)
+console.log(page)
 
 ReactDOM.render(
-    navbar,
+    page,
     document.getElementById("root"))
