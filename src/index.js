8fc72376fe9166156d3fdf1ea312070953272d5f
Pedro Mota <pedro.mota@exaud.com>
2022-06-30 15:39:50 +0100

    8.2 - added paragraph element to new constant page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-const h1 = document.createElement("h1")
-h1.textContent = "Hello world"
-h1.className = "header"
-console.log(h1)
+// const h1 = document.createElement("h1")
+// h1.textContent = "Hello world"
+// h1.className = "header"
+// console.log(h1)
 
-const element = <h1 className="header"> This is JSX</h1>
-console.log(element)
+const page = (
+    <div>
+        <h1 className="header">This is JSX</h1>
+        <p>This is a paragraph</p>
+    </div>
+)
+
+console.log(page)
 
 ReactDOM.render(
-    element,
+    page,
     document.getElementById("root"))
