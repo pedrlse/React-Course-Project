3be84a3a891e63a01f8bd3ed509021a4d585970a
Pedro Mota <pedro.mota@exaud.com>
2022-06-30 15:47:41 +0100

    8.3 - changed constant displayed to navbar

--- src/index.js
+++ src/index.js
@@ -3,15 +3,28 @@
 // h1.className = "header"
 // console.log(h1)
 
-const page = (
-    <div>
-        <h1 className="header">This is JSX</h1>
-        <p>This is a paragraph</p>
-    </div>
+// const page = (
+//     <div>
+//         <h1 className="header">This is JSX</h1>
+//         <p>This is a paragraph</p>
+//     </div>
+// )
+
+const navbar = (
+    <nav>
+        <h1>Bob's Bistro</h1>
+        <ul>
+            <li>Menu</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
 )
 
 console.log(page)
 
+console.log(navbar)
+
 ReactDOM.render(
-    page,
+    navbar,
     document.getElementById("root"))
