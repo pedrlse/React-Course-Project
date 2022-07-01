475bd558057061e938075957185406f5ac1c684c
Pedro Mota <pedro.mota@exaud.com>
2022-07-01 10:59:19 +0100

    18.3. moved MainContent from index.js to MainContent.js

---
+++ src/MainContent.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default
+function MainContent() {
+    return (
+        <div className="main-content">
+            <h1>Fun Facts about React</h1>
+            <ol>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>has well over 100k stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ol>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom"
 import css from "./index.css"
 import Header from "./Header"
 import Footer from "./Footer"
+import MainContent from "./MainContent"
 
 
 function Page() {
@@ -11,21 +12,6 @@ function Page() {
             <Header />
             <MainContent />
             <Footer />
-        </div>
-    )
-}
-
-function MainContent() {
-    return (
-        <div className="main-content">
-            <h1>Fun Facts about React</h1>
-            <ol>
-                <li>Was first released in 2013</li>
-                <li>Was originally created by Jordan Walke</li>
-                <li>has well over 100k stars on GitHub</li>
-                <li>Is maintained by Facebook</li>
-                <li>Powers thousands of enterprise apps, including mobile apps</li>
-            </ol>
         </div>
     )
 }
