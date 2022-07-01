03ef808b7b0409662a30fa77164b1b4d1e79b551
Pedro Mota <pedro.mota@exaud.com>
2022-07-01 16:09:33 +0100

    7.1. arranged items in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,11 +7,11 @@ function Header() {
     return(
         <nav>
             <img src= {img} className= "nav--image"/>
-            <h1>Laura Smith</h1>
-            <h2>Frontend Developer</h2>
-            <h3>laurasmith.website</h3>
-            <button><h1>Email</h1></button>
-            <button><h1>LinkedIn</h1></button>
+            <h1 className="nav--name">Laura Smith</h1>
+            <h2 className="nav--job">Frontend Developer</h2>
+            <h3 className="nav--website">laurasmith.website</h3>
+            <button className="button--mail"><h1>Email</h1></button>
+            <button className="button--linkedin"><h1>LinkedIn</h1></button>
         </nav>
     );
 }
