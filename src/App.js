b553838a9968e8641a1616346c98dea02bba5688
Pedro Mota <pedro.mota@exaud.com>
2022-07-01 14:53:28 +0100

    3.1. created MainContent

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Navbar from "./components/Navbar"
+import MainContent from "./components/MainContent";
 export default function App() {
   return (
     <div>
       <Navbar />
+      <MainContent />
     </div>
   );
 }
---
+++ src/components/MainContent.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default
+function MainContent() {
+    return(
+        <nav>
+            <h2>Laura Smith</h2>
+            <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+            <h2>Interests</h2>
+            <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+        </nav>
+    );
+}
