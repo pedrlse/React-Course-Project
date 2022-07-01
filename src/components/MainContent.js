54fdecf4d30409ce73382b012e48e4f4c82f937f
Pedro Mota <pedro.mota@exaud.com>
2022-07-01 16:32:57 +0100

    7.2. arranged items in main

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -4,10 +4,10 @@ export default
 function MainContent() {
     return(
         <main>
-            <h2>Laura Smith</h2>
+            <h2 className="main--about">Laura Smith</h2>
             <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
-            <h2>Interests</h2>
-            <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+            <h2 className="main--interests">Interests</h2>
+            <p style={{paddingTop: "100px"}}> Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
         </main>
     );
 }
