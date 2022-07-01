25a1167161b9a30d84a36b297e30d3d430ab5a5d
Pedro Mota <pedro.mota@exaud.com>
2022-07-01 16:56:11 +0100

    7.3. arranged items in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,16 @@
 import React from "react"
-
+import twitter from '../Twitter Icon.png'
+import github from '../GitHub Icon.png'
+import facebook from '../Facebook Icon.png'
+import instagram from '../Instagram Icon.png'
 export default
 function Footer() {
     return(
-        <footer>
-            <img />
-            <img />
-            <img />
-            <img />
+        <footer className="icons">
+            <img src = {twitter} className="twitter"/>
+            <img src = {github} className = "github"/>
+            <img src = {facebook} className = "facebook"/>
+            <img src = {instagram} className = "instagram"/>
         </footer>
     );
 }
