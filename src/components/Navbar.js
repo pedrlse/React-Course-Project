b095a0c17dfaa378cb53a122b902502e0bff2e90
Pedro Mota <pedro.mota@exaud.com>
2022-07-01 15:15:44 +0100

    5.2. cropped image and changed style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
 import React from "react"
 import img from "../image.png"
+import "../index.css"
 
 export default
 function Header() {
     return(
         <nav>
-            <img src= {img}/>
+            <img src= {img} className= "nav--image"/>
             <h1>Laura Smith</h1>
             <h2>Frontend Developer</h2>
             <h3>laurasmith.website</h3>
