806ab0a276b1b750676483c9647765ceacf90072
Pedro Mota <pedro.mota@exaud.com>
2022-07-01 17:12:15 +0100

    8.1. moved content from Navbar to MainContent

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -4,6 +4,11 @@ export default
 function MainContent() {
     return(
         <main>
+            <h1 className="main--name">Laura Smith</h1>
+            <h2 className="main--job">Frontend Developer</h2>
+            <h3 className="main--website">laurasmith.website</h3>
+            <button className="button--mail"><h1>Email</h1></button>
+            <button className="button--linkedin"><h1>LinkedIn</h1></button>
             <h2 className="main--about">Laura Smith</h2>
             <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
             <h2 className="main--interests">Interests</h2>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,11 +7,6 @@ function Header() {
     return(
         <nav>
             <img src= {img} className= "nav--image"/>
-            <h1 className="nav--name">Laura Smith</h1>
-            <h2 className="nav--job">Frontend Developer</h2>
-            <h3 className="nav--website">laurasmith.website</h3>
-            <button className="button--mail"><h1>Email</h1></button>
-            <button className="button--linkedin"><h1>LinkedIn</h1></button>
         </nav>
     );
 }
