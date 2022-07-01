4be7e8210777519481c6a627f95a1bfd2711ec0a
Pedro Mota <pedro.mota@exaud.com>
2022-07-01 11:48:51 +0100

    24.4. rearranged Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,10 @@ import logo from "../react-icon-small.png";
 
 export default function Header() {
   return (
-    <header>
       <nav className="nav">
-        <img src={logo} className="nav-logo" />
-        <h3>ReactFacts</h3>
-        <h4>React Course - Project 1</h4>
+        <img src={logo} className="nav--icon" />
+        <h3 className="nav--logo_text">ReactFacts</h3>
+        <h4 className="nav--title">React Course - Project 1</h4>
       </nav>
-    </header>
   );
 }
