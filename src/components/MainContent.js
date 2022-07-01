dc8e1a93f37d4cd0681d6ef9b39a4954b81fa851
Pedro Mota <pedro.mota@exaud.com>
2022-07-01 12:02:23 +0100

    25.4. replaced ordered list with an unordered list

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -5,13 +5,13 @@ function MainContent() {
     return (
         <main>
             <h1 className="main--title">Fun Facts about React</h1>
-            <ol>
+            <ul className="main--facts">
                 <li>Was first released in 2013</li>
                 <li>Was originally created by Jordan Walke</li>
                 <li>has well over 100k stars on GitHub</li>
                 <li>Is maintained by Facebook</li>
                 <li>Powers thousands of enterprise apps, including mobile apps</li>
-            </ol>
+            </ul>
         </main>
     )
 }
