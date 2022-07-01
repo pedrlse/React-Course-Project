9986b79470fa072e7c2c106db7419e04a63ce91f
Pedro Mota <pedro.mota@exaud.com>
2022-07-01 15:00:40 +0100

    4.1. created Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar"
 import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
 export default function App() {
   return (
     <div>
       <Navbar />
       <MainContent />
+      <Footer />
     </div>
   );
 }
