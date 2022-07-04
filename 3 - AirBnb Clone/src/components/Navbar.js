85837189a85e2a01d2a50e8347c80334c9842e92
Pedro Mota <pedro.mota@exaud.com>
2022-07-04 15:03:34 +0100

    3.1.6. applied style to Navbar logo

--- 3 - AirBnb Clone/src/components/Navbar.js
+++ 3 - AirBnb Clone/src/components/Navbar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import reactlogo from "../airbnb-logo.png";
+import css from "../style.css";
 
 export default function Navbar() {
     return (
         <nav>
-            <img src= {reactlogo} />
+            <img src= {reactlogo} className="nav--logo"/>
         </nav>
     )
 }
