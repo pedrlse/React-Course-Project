0bc1f46b38acc6bdd4602d7f5a9964c9f36f0cca
Pedro Mota <pedro.mota@exaud.com>
2022-07-04 13:03:38 +0100

    3.1.3. imported Navbar to App.js

--- 3 - AirBnb Clone/src/App.js
+++ 3 - AirBnb Clone/src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
-    <h1>App component</h1>
+    
+    <div>
+      <h1>App component</h1>
+      <Navbar />
+    </div>
   )
 }
