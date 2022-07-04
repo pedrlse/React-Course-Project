60c57beb961c5bbc5210e23826dc8dc9d57cfcb8
Pedro Mota <pedro.mota@exaud.com>
2022-07-04 12:59:08 +0100

    3.1.2. added Navbar component in new js file

---
+++ 3 - AirBnb Clone/src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import reactlogo from "../airbnb-logo.png";
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src= {reactlogo} />
+        </nav>
+    )
+}
