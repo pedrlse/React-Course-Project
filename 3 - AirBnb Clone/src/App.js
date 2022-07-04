90402e983fad65a37d565defafbc75bdffc7238e
Pedro Mota <pedro.mota@exaud.com>
2022-07-04 18:02:13 +0100

    3.15.1. changed Card component to use props

--- 3 - AirBnb Clone/src/App.js
+++ 3 - AirBnb Clone/src/App.js
@@ -2,13 +2,21 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
+import katie from "./katie-zaferes.png";
 
 export default function App() {
   return (
     <div>
       <Navbar />
       <Hero />
-      <Card />
+      <Card 
+        img={katie}
+        rating="5.0"
+        reviewCount={6}
+        country="USA"
+        title="Life Lessons with Katie Zaferes"
+        price={136}
+      />
     </div>
   )
 }
