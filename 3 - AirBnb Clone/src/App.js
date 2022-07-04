403c2d5c49d0120c1edc31d1b1dfd37a273a2a65
Pedro Mota <pedro.mota@exaud.com>
2022-07-04 18:58:57 +0100

    3.21.2. loaded info and images from data.js with .map

--- 3 - AirBnb Clone/src/App.js
+++ 3 - AirBnb Clone/src/App.js
@@ -3,20 +3,27 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
 import katie from "./katie-zaferes.png";
+import data from "./data";
 
+console.log(data);
 export default function App() {
+
+  const cards = data.map(item => {
+    return (
+        <Card 
+          img={item.coverImg}
+          rating={item.stats.rating}
+          reviewCount={item.stats.reviewCount}
+          location={item.location}
+          title={item.title}
+          price={item.price}
+        />
+    )
+  })
+
   return (
     <div>
       <Navbar />
-      <Hero />
-      <Card 
-        img={katie}
-        rating="5.0"
-        reviewCount={6}
-        country="USA"
-        title="Life Lessons with Katie Zaferes"
-        price={136}
-      />
     </div>
   )
 }
