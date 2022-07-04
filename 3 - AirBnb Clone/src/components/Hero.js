289df16fb008fa1f79c8720c172f1f428858c7c3
Pedro Mota <pedro.mota@exaud.com>
2022-07-04 15:27:00 +0100

    3.2.2. added style to hero component

--- 3 - AirBnb Clone/src/components/Hero.js
+++ 3 - AirBnb Clone/src/components/Hero.js
@@ -5,8 +5,8 @@ export default function Hero() {
     return (
         <section>
             <img src= {herophoto} className="hero--photo"/>
-            <h1>Online Experiences</h1>
-            <p>Join unique interactive activities led by one-of-a-kind hosts-all without leaving home.</p>
+            <h1 className="hero--header">Online Experiences</h1>
+            <p className="hero--text">Join unique interactive activities led by one-of-a-kind hosts-all without leaving home.</p>
         </section>
     )
 }
