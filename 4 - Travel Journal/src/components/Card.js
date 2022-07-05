3dfee5774fb147fd4f6ba9810f39aaedf00bbccd
Pedro Mota <pedro.mota@exaud.com>
2022-07-05 16:05:35 +0100

    4.6. changed Card component

--- 4 - Travel Journal/src/components/Card.js
+++ 4 - Travel Journal/src/components/Card.js
@@ -1,11 +1,18 @@
 import React from "react";
+import pointer from "../Pointer.png"
 
 export default function Card(props)
 {
     return (
-        <div>
-            <div className="card">
-                <img src= {props.item.imageUrl} />
+        <div  className="card">
+            <img src= {props.item.imageUrl} className= "card--image" />
+            <div>
+                <img src={pointer} className="card--pointer"/>
+                <span className="card--location">{props.item.location}</span>
+                <a className="card--url" href={props.item.googleMapsUrl}>View in Google Maps</a>
+                <p className="card--title">{props.item.title}</p>
+                <p className="card--date">{props.item.startDate} - {props.item.endDate}</p>
+                <p className="card--text">{props.item.description}</p>
             </div>
         </div>
     )
