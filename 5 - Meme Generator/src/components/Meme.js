a99ca6bbd3ef3ee3bb0420d263fc73ae6e6322cf
Pedro Mota <pedro.mota@exaud.com>
2022-07-05 18:39:35 +0100

    5.6.3. call memesData file from the Meme component

--- 5 - Meme Generator/src/components/Meme.js
+++ 5 - Meme Generator/src/components/Meme.js
@@ -1,7 +1,20 @@
 import React from "react";
+import memesData from "../memesData.js"
+
 export default function Meme() {
+
+    let url
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        url = memesArray[randomNumber].url
+        console.log(url)
+    }
+    
     return (
         <main>
+            <p>{url}</p>
             <div className="form">
                 <input 
                     type="text"
@@ -15,6 +28,7 @@ export default function Meme() {
                 />
                 <button
                     className="form--button"
+                    onClick={getMemeImage}
                 >
                     Get a new meme image 🖼
                 </button>
