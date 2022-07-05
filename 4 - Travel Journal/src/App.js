a46b1061d748a6e415e0d333e0a3c562bc688576
Pedro Mota <pedro.mota@exaud.com>
2022-07-05 14:40:20 +0100

    4.3. added logo to navbar

--- 4 - Travel Journal/src/App.js
+++ 4 - Travel Journal/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import css from "./style.css"
+import logo from "./Logo.png";
 
 export default function App() {
     return (
         <nav>
+            <img src={logo} className="navbar--logo"></img>
             <h2>my travel journal.</h2>
         </nav>
     )
