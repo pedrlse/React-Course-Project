a002d5ca64049574d28a80aea9768120de58f64a
Pedro Mota <pedro.mota@exaud.com>
2022-07-05 12:42:06 +0100

    4.2. created App.js file

---
+++ 4 - Travel Journal/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import css from "./style.css"
+
+export default function App() {
+    return (
+        <nav>
+            <h2>my travel journal.</h2>
+        </nav>
+    )
+}
