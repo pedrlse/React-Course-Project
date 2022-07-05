c9faf7cbdef9b974925e266c556a7592be8d6968
Pedro Mota <pedro.mota@exaud.com>
2022-07-05 18:28:44 +0100

    5.6.2. changed form element to be a div instead

--- 5 - Meme Generator/src/components/Meme.js
+++ 5 - Meme Generator/src/components/Meme.js
@@ -2,7 +2,7 @@ import React from "react";
 export default function Meme() {
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <input 
                     type="text"
                     placeholder="Top text"
@@ -18,7 +18,7 @@ export default function Meme() {
                 >
                     Get a new meme image 🖼
                 </button>
-            </form>
+            </div>
         </main>
     )
 }
