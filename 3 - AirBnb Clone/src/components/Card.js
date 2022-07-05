d23356757d5674becd0a8278a548999106e504c9
Pedro Mota <pedro.mota@exaud.com>
2022-07-05 11:17:54 +0100

    3.23.4. added badge display on each card element

--- 3 - AirBnb Clone/src/components/Card.js
+++ 3 - AirBnb Clone/src/components/Card.js
@@ -3,15 +3,22 @@ import star from "../star.png"
 
 export default function Card(props)
 {
+    let badgeText
+    if (props.openSpots === 0){
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    }
+
     return (
         <div className="card">
-        <div className="card--badge">SOLD OUT</div>
+        {badgeText && <div className="card--badge">{badgeText}</div>}
             <img src={props.img} className="card--image"/>
             <div className="card--text">
                 <img src={star} className="card--star"/>
                 <span>{props.rating}</span>
                 <span className="gray">({props.reviewCount}) • </span>
-                <span className="gray">{props.country}</span>
+                <span className="gray">{props.location}</span>
             </div>
             <p>{props.title}</p>
             <p><span className="bold">From ${props.price}</span> / person</p>
