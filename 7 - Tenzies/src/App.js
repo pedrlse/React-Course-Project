fd41b7e1bb712b97addc6f09a7819381adae2774
Pedro Mota <pedrlse3@gmail.com>
2022-07-12 14:35:22 +0100

    7.4. created map array to randomize die value

--- 7 - Tenzies/src/App.js
+++ 7 - Tenzies/src/App.js
@@ -2,19 +2,23 @@ import React from "react";
 import Die from "./Die";
 
 export default function App() {
+    const [dice, setDice] = React.useState(allNewDice())
+
+    function allNewDice (){
+        const newDice = []
+        for (let i = 0; i < 10; i++) {
+            newDice.push(Math.ceil(Math.random() * 6))
+        }
+        return newDice
+    }
+    console.log(allNewDice())
+
+    const diceElements = dice.map(die => <Die value={die}/>)
+
     return (
         <main>
             <div className="dice-container">
-                <Die value="1" />
-                <Die value="2" />
-                <Die value="3" />
-                <Die value="4" />
-                <Die value="5" />
-                <Die value="6" />
-                <Die value="7" />
-                <Die value="8" />
-                <Die value="9" />
-                <Die value="0" />
+                {diceElements}
             </div>
         </main>
     )
